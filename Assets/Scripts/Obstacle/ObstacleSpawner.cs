f9ac7925bd17bd4be3fa85f66150c0d35b6bb440
JayJeongJW <p1fixer2@gmail.com>
2025-05-09 14:12:55 +0900

    장애물 프리팹화
    장애물 프리팹화

--- Assets/Scripts/Obstacle/ObstacleSpawner.cs
+++ Assets/Scripts/Obstacle/ObstacleSpawner.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
+namespace runner
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ObstacleSpawner : MonoBehaviour
     {
-        
-    }
+        [Header("장애물 프리팹들")]
+        public GameObject[] obstaclePrefabs;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("장애물 생성 위치")]
+        public Vector3[] spawnPositions;
+
+        private int currentStage = 0; //스테이지 확장 인덱스
+
+        void Start()
+        {
+            SpawnObstaclesForStage(currentStage);
+        }
+
+        public void SpawnObstaclesForStage(int stageNum)
+        {
+            for (int i = 0; i < spawnPositions.Length; i++)
+            {
+                //장애물 타입을 자동으로 순서대로 바뀜
+                ObstacleType type = (ObstacleType)(i % System.Enum.GetNames(typeof(ObstacleType)).Length);
+
+                GameObject prefab = obstaclePrefabs[(int)type]; //장애물 타입에 맞는 프리팹 선택
+                GameObject obj = Instantiate(prefab, spawnPositions[i], Quaternion.identity); //지정된 위치에 장애물 생성
+
+                Obstacle obstacle = obj.GetComponent<Obstacle>(); 
+                if (obstacle != null)
+                {
+                    obstacle.obstacleType = type; //장애물 타입 설정
+                    obstacle.SetSpawnIndex(i); //몇번째 장애물인지 설정
+                }
+            }
+        }
     }
 }
