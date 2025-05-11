bcfa938882fb50530586980bb76d994d33a018e4
odh1012 <odh1012@naver.com>
2025-05-12 00:35:13 +0900

    아이템 태그 추가 , 플레이어가 아이템 습득가능
    아이템에는 아이템 태그 필수

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 5f;
-    private Rigidbody2D playrigidboody;
+    private Rigidbody2D Playerrigidbody;
     private Animator animator;
 
     public int maxJumpCount = 2;
@@ -13,20 +13,18 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        playrigidboody = GetComponent<Rigidbody2D>();
+        Playerrigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
     }
-
     void Update()
     {
         // 점프
         if (Input.GetKeyDown(KeyCode.Z) && currentJumpCount < maxJumpCount && !Input.GetKey(KeyCode.X))
         {
-            playrigidboody.velocity = new Vector2(playrigidboody.velocity.x, jumpForce);
+            Playerrigidbody.velocity = new Vector2(Playerrigidbody.velocity.x, jumpForce);
             animator.SetBool("IsJump", true);
             currentJumpCount++;
         }
-
         // 슬라이딩 (X키 누르고 있을 때)
         if (Input.GetKey(KeyCode.X))
         {
@@ -38,17 +36,23 @@ public class PlayerController : MonoBehaviour
         }
 
         // 떨어질 때 점프 애니메이션 끄기
-        if (playrigidboody.velocity.y <= 0)
+        if (Playerrigidbody.velocity.y <= 0)
         {
             animator.SetBool("IsJump", false);
         }
     }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             currentJumpCount = 0;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            Destroy(other.gameObject); // 태그에'아이템'이 붙어있는 오브젝트와 충돌하면 충돌한 아이템이 사라짐
         }
     }
 }
--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -7,17 +7,14 @@ public class PlayerStatus : MonoBehaviour
     public float maxHP = 100f;
     public float currentHP;
     public float hpDecreaseRate = 5f; // 초당 감소할 HP량
-
     void Start()
     {
         currentHP = maxHP;
     }
-
     void Update()
     {
         // 시간 경과에 따라 HP 감소
         currentHP -= hpDecreaseRate * Time.deltaTime;
-
         // HP가 0 이하가 되면 사망
         if (currentHP <= 0)
         {
@@ -25,7 +22,6 @@ public class PlayerStatus : MonoBehaviour
             Die();
         }
     }
-
     void Die()
     {
         //캐릭터 죽으면 UI호출
