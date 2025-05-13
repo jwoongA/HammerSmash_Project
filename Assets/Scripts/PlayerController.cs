f9535b2ffa44679948492ff6494c150b0f411352
odh1012 <odh1012@naver.com>
2025-05-13 12:28:29 +0900

    점프 슬라이딩 소리 추가
    배경음으로 쓸 에셋 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,18 @@ public class PlayerController : MonoBehaviour
 
     private PlayerStatus playerStatus;
 
+    public AudioClip firstJumpSound; // 1단 점프 소리
+    public AudioClip doubleJumpSound; // 2단 점프 소리
+    private AudioSource audioSource;
+
+    public AudioClip slidingSound; // 슬라이딩 소리
+    private bool hasPlayedSlidingSound = false;
+
     void Start()
     {
         Playerrigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponent<AudioSource>();
 
         playerStatus = GetComponent<PlayerStatus>();
         if (playerStatus == null)
@@ -33,16 +41,38 @@ public class PlayerController : MonoBehaviour
             Playerrigidbody.velocity = new Vector2(Playerrigidbody.velocity.x, jumpForce);
             animator.SetBool("IsJump", true);
             animator.SetBool("IsSliding", false);
+
             currentJumpCount++;
+
+            // 점프 횟수에 따라 다른 소리 재생
+            if (audioSource != null)
+            {
+                if (currentJumpCount == 1 && firstJumpSound != null)
+                {
+                    audioSource.PlayOneShot(firstJumpSound);
+                }
+                else if (currentJumpCount == 2 && doubleJumpSound != null)
+                {
+                    audioSource.PlayOneShot(doubleJumpSound);
+                }
+            }
         }
         // 슬라이딩
         else if (Input.GetKey(KeyCode.X))
         {
             animator.SetBool("IsSliding", true);
+
+            // 사운드 1회만 재생
+            if (!hasPlayedSlidingSound && slidingSound != null)
+            {
+                audioSource.PlayOneShot(slidingSound);
+                hasPlayedSlidingSound = true;
+            }
         }
         else
         {
             animator.SetBool("IsSliding", false);
+            hasPlayedSlidingSound = false; // X 키에서 손 떼면 다시 재생 가능하게
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
