f835cbdcdb3743f845235b94a12d15536f534d3d
Delluu <golvlog854@gmail.com>
2025-05-14 14:21:24 +0900

    시간과 점수 연결되게 코드 수정중

--- Assets/UI/ScoreDataBuffer.cs
+++ Assets/UI/ScoreDataBuffer.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public static class ScoreDataBuffer
 {
+    // 게임이 끝났을 때 기록용
     public static int FinalScore;
     public static float FinalTime;
-} // 스코어를 기록하는 용
+
+    // 씬 이동 중 유지용
+    public static int CurrentScore;     
+    public static float CurrentTime;
+} // 스코어를 기록
--- Assets/UI/ScoreUI.cs
+++ Assets/UI/ScoreUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreUI : MonoBehaviour
 {
@@ -10,6 +11,26 @@ public class ScoreUI : MonoBehaviour
 
     void Start()
     {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene == "Stage1_Scene" || currentScene == "Loding2_Scene" || currentScene == "Stage2_Scene")
+        {
+            if (playerEffect != null)
+            {
+                playerEffect.score = ScoreDataBuffer.CurrentScore;
+                Debug.Log($"[ScoreUI] 이전 점수 유지됨: {playerEffect.score}");
+            }
+        }
+        else
+        {
+            if (playerEffect != null)
+            {
+                playerEffect.score = 0;
+                ScoreDataBuffer.CurrentScore = 0;
+                Debug.Log("[ScoreUI] 점수 초기화됨");
+            }
+        }
+
         if (scoreText != null)
             scoreText.text = "SCORE : 0";
     }
@@ -19,9 +40,7 @@ public class ScoreUI : MonoBehaviour
         if (playerEffect != null && scoreText != null)
         {
             scoreText.text = $"{playerEffect.score.ToString("D4")}";
-
-
-
+            ScoreDataBuffer.CurrentScore = playerEffect.score;
         }
     }
 }
